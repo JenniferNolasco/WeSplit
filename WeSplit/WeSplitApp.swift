9f321c70e5a86ec0aae2b001d650c6aecde9cec6
Jennifer Nolasco <nolasco.jennifer00@gmail.com>
2021-06-20 14:50:06 -0400

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Grace on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
