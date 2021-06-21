6cd00dac097a342ea67b91e959da4be5b703f69c
Jennifer Nolasco <nolasco.jennifer00@gmail.com>
2021-06-20 21:10:33 -0400

    began adding initial variables

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -1,21 +1,33 @@
-//
-//  ContentView.swift
-//  WeSplit
-//
-//  Created by Grace on 6/20/21.
-//
+
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount: String = ""
+    @State private var tipIndex: Int = 2
+    @State private var peopleAmount: Int = 2
+    let tipAmount = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      
+        Form {
+            Section {
+                TextField ("Enter the cost of food", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+            }
+            Section {
+                Text("Check Amount: $\(checkAmount)")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.light)
+        
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
