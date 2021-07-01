fa22195ecbc2d823f12c99223d34c8e1b927f88c
Jennifer Nolasco <nolasco.jennifer00@gmail.com>
2021-07-01 10:06:15 -0400

    Calculated check amounts

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -3,16 +3,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var checkAmount: String = ""
+    @State private var billAmount: String = ""
     @State private var tipIndex: Int = 2
-    @State private var peopleAmount: Int = 2
+    @State private var peopleAmount: Int = 0
     let tipAmount = [10, 15, 20, 25, 0]
+    
+    var checkTotal: Double {
+        let tipPercentage = Double(tipAmount[tipIndex]) / 100
+        let billPrice = Double(billAmount) ?? 0
+        let addTip = Double(billPrice * tipPercentage)
+        
+        return billPrice + addTip
+    }
+    
+    var checkAmount: Double {
+        let peopleCount = Double(peopleAmount + 2)
+        let tipPercentage = Double(tipAmount[tipIndex]) / 100
+        let billPrice = Double(billAmount) ?? 0
+        let addTip = Double((billPrice / peopleCount) * tipPercentage)
+        
+        return (billPrice / peopleCount) + addTip
+    }
     
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField ("Bill Amount", text: $checkAmount)
+                    TextField ("Bill Amount", text: $billAmount)
                         .keyboardType(.decimalPad)
                     Picker ("Number of People", selection: $peopleAmount) {
                         ForEach (2..<101) { people in
@@ -28,8 +45,11 @@ struct ContentView: View {
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
-                Section {
-                    Text("Check Amount: $\(checkAmount)")
+                Section(header: Text("Total Check with tip")) {
+                    Text("$\(checkTotal, specifier: "%.2f")")
+                }
+                Section(header: Text("Amount per person")) {
+                    Text("$\(checkAmount, specifier: "%.2f")")
                 }
             }
             .navigationBarTitle("We Split")
